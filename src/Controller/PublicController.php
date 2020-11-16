6754c5a960ac679b9c1c634130940a234187cceb
Mathieu Bonhommeau <mathieu.bonhommeau@orange.fr>
2020-11-16 21:17:17 +0100

    add comment form is OK

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\TricksRepository;
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PublicController extends AbstractController
@@ -35,32 +39,59 @@ class PublicController extends AbstractController
     /**
      * @Route("/tricks/{name}", name="show_tricks")
      */
-    public function show(TricksRepository $tricksRepository, MessageRepository $messageRepository, $name, Request $request)
-    {
+    public function show(TricksRepository $tricksRepository,  
+        MessageRepository $messageRepository, $name, Request $request,
+        EntityManagerInterface $manager, UserInterface $user = null
+    ) {
+        //We get id of tricks by slug 'name' in the route
         $id = $tricksRepository->findTricksIdByName($name);
 
-        if ($request->isXmlHttpRequest()) {
+        //We create form for add a new message
+        $newMessage = new Message();
+        $messageForm = $this->createForm(MessageType::class, $newMessage);
 
+        //Display more messages in ajax
+        if ($request->isXmlHttpRequest()) {
             $offset = (($request->request->get('numPage'))-1)*10;
             
             $nextMessages = $messageRepository->findByTricks($id, 10, $offset);
             $jsonResponse =$this->render('public/nextMessages.html.twig', [
                 'nextMessages' => $nextMessages
             ]);
-
             return new JsonResponse(['body' => $jsonResponse->getContent()]);
-           
         }
 
+        //We get the tricks and all tricks messages
         $tricks = $tricksRepository->find($id);
 
         $numberMessages = $messageRepository->countByTricks($id);
         $messages = $messageRepository->findByTricks($id, 10, 0);
 
+        //Addmessage form process and check if the user is authenticate
+        $messageForm->handleRequest($request);
+        if ($user && $messageForm->isSubmitted() && $messageForm->isValid()) {
+            
+            //We check if the user account is activate
+            if ($user->getActivationToken() != '') {
+                throw new \Exception('Vous devez activez votre compte !!');
+            }
+
+            $newMessage->setUser($user)
+                       ->setDateMessage(new \Datetime())
+                       ->setTricks($tricks);
+            $manager->persist($newMessage);
+            $manager->flush();
+
+            return $this->redirectToRoute('show_tricks', [
+                'name' => $tricks->getName()
+            ]);
+        }
+
         return $this->render('public/show.html.twig', [
             'tricks' => $tricks,
             'messages' => $messages,
-            'numberMessages' => (int)$numberMessages
+            'numberMessages' => (int)$numberMessages,
+            'messageForm' => $messageForm->createView()
         ]);
     }
 }
