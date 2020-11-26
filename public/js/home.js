bb6c1ec0dab54d3cb6f579e10b4555606afc207c
Mathieu Bonhommeau <mathieu.bonhommeau@orange.fr>
2020-11-26 23:54:46 +0100

    finish responsive

--- public/js/home.js
+++ public/js/home.js
@@ -8,7 +8,12 @@ window.onscroll = function () {
         document.getElementById("homeMenu").style.borderBottom = "1px solid rgba(26, 26, 26)";
         document.getElementById("homeMenu").style.boxShadow = "0 .5rem 1rem rgba(0,0,0,.15)";
     } else {
-        document.getElementById("homeMenu").style.backgroundColor = "rgba(255, 255, 255, 0)";
+        
+        if (typeof $('.navbar-toggler-icon') != undefined) {
+            document.getElementById("homeMenu").style.backgroundColor = "rgba(247, 247, 247, 0.8)";
+        } else {
+            document.getElementById("homeMenu").style.backgroundColor = "rgba(255, 255, 255, 0)";
+        }
         document.getElementById("homeMenu").style.borderBottom = "none";
         document.getElementById("homeMenu").style.boxShadow = "none";
     }
